9221e6c02906b1319964c8e676a3168224dc2726
étudiant <etudiant@IMAC-2018-MODELE-51.local>
2020-10-14 17:14:32 +0200

    git commit

---
+++ Sound/anim.js
@@ -0,0 +1,39 @@
+AFRAME.registerComponent('switch', {
+  init: function () {
+    var el = this.el;
+    var change = 0;
+    var vitesse = 0.2
+    var save = 0;
+
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;};
+
+    el.addEventListener('animation-finished', () => {
+
+      change = getRandomInt(1,5)
+
+
+      switch(change)
+      {
+        case 1: el.setAttribute("animation-mixer" , "clip: pas-01-d");
+        break;
+        case 2: el.setAttribute("animation-mixer" , "clip: pas-02-f");
+        break;
+        case 3: el.setAttribute("animation-mixer" , "clip: pas-03-f");
+        break;
+        case 4: el.setAttribute("animation-mixer" , "clip: pas-04-f");
+        break;
+        case 5: el.setAttribute("animation-mixer" , "clip: pas-05-f");
+        break;
+        default : el.setAttribute("animation-mixer" , "clip: pas-01-d");
+        break;
+
+
+      };
+
+
+      });
+    }
+});
