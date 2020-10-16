6706a614650568a914cff1235ed067cef1bbe3d9
étudiant <etudiant@IMAC-2018-MODELE-51.local>
2020-10-16 14:54:06 +0200

    git commit

--- Sound/anim.js
+++ Sound/anim.js
@@ -1,5 +1,7 @@
 AFRAME.registerComponent('switch', {
   init: function () {
+    var i=1
+    var j=0
     var el = this.el;
     var change = 0;
     var vitesse = 0.2
@@ -11,6 +13,53 @@ AFRAME.registerComponent('switch', {
       return Math.floor(Math.random() * (max - min + 1)) + min;};
 
     el.addEventListener('animation-finished', () => {
+
+
+      if (i<5) {
+
+        el.setAttribute("animation-mixer" , "repetitions : 2");
+
+        i++
+        switch(i)
+        {
+          case 1: el.setAttribute("animation-mixer" , "clip: pas-01-d");
+          break;
+          case 2: el.setAttribute("animation-mixer" , "clip: pas-02-f");
+          break;
+          case 3: el.setAttribute("animation-mixer" , "clip: pas-03-f");
+          break;
+          case 4: el.setAttribute("animation-mixer" , "clip: pas-04-f");
+          break;
+          case 5: el.setAttribute("animation-mixer" , "clip: pas-05-f");
+          break;
+          default : el.setAttribute("animation-mixer" , "clip: pas-01-d");
+          break;
+        };
+
+      } else {
+
+        if (j<5) {
+
+          el.setAttribute("animation-mixer" , "repetitions : 1");
+
+          j++
+          switch(j)
+          {
+            case 1: el.setAttribute("animation-mixer" , "clip: pas-01-d");
+            break;
+            case 2: el.setAttribute("animation-mixer" , "clip: pas-02-f");
+            break;
+            case 3: el.setAttribute("animation-mixer" , "clip: pas-03-f");
+            break;
+            case 4: el.setAttribute("animation-mixer" , "clip: pas-04-f");
+            break;
+            case 5: el.setAttribute("animation-mixer" , "clip: pas-05-f");
+            break;
+            default : el.setAttribute("animation-mixer" , "clip: pas-01-d");
+            break;
+          };
+
+        } else {
 
       save = change;
 
@@ -44,6 +93,10 @@ AFRAME.registerComponent('switch', {
 
 
       };
+
+    }
+
+  }
 
 
       });
